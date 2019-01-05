958bc70ff538d679672959d09b11ba658ad3a727
Beth Griggs <Bethany.Griggs@uk.ibm.com>
2019-01-05 23:03:58 +0000

    css: implement close window

--- script.js
+++ script.js
@@ -3,3 +3,14 @@ function updateTime() {
 }
 
 setInterval(updateTime, 1000);
+
+
+
+/* Window Events */
+
+document.getElementById("aboutme-close").addEventListener('click', function () {
+    document.getElementById("aboutme").hidden = true;
+    setInterval(function () {
+        document.getElementById("aboutme").hidden = false
+    }, 500);
+});
