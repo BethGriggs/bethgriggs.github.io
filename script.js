01e51bc6aaa0a4f28b4179dc46b0b461e325519f
Beth Griggs <Bethany.Griggs@uk.ibm.com>
2019-03-28 22:37:59 +0000

    script: swap to arrow functions

--- script.js
+++ script.js
@@ -8,9 +8,9 @@ setInterval(updateTime, 1000);
 
 /* Window Events */
 
-document.getElementById("aboutme-close").addEventListener('click', function () {
+document.getElementById("aboutme-close").addEventListener('click', () => {
     document.getElementById("aboutme").hidden = true;
-    setInterval(function () {
+    setInterval(() => {
         document.getElementById("aboutme").hidden = false
     }, 500);
 });
